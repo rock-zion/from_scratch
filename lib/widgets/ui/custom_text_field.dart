b50c67a584e88b4d48478e715bbcdf54a864a246
rockxzion <rock_zion@outlook.com>
2023-05-19 09:41:09 +0100

    Create custom_text_field.dart

---
+++ lib/widgets/ui/custom_text_field.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:from_scratch/constants.dart';
+
+class CustomTextField extends StatefulWidget {
+  const CustomTextField({
+    this.obscure = false,
+    this.inputType = TextInputType.none,
+    required this.label,
+    super.key,
+  });
+
+  final bool obscure;
+  final String label;
+  final TextInputType inputType;
+
+  @override
+  _CustomTextFieldState createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      keyboardType: widget.inputType,
+      obscureText: widget.obscure,
+      decoration: InputDecoration(
+          // focusedBorder: const InputBorder(borderSide: BorderSide()),
+          // border: const InputBorder(),
+          label: Text(
+            widget.label,
+            style: Theme.of(context).textTheme.bodyMedium,
+          ),
+          border: const UnderlineInputBorder(
+              borderSide: BorderSide(color: kGreen, width: 1)),
+          focusedBorder: const UnderlineInputBorder(
+              borderSide: BorderSide(color: kGreen, width: 1))),
+    );
+  }
+}
