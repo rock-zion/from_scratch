ae372b1cc862fd239f79f05e779c9a2fa4f46ed2
rockxzion <rock_zion@outlook.com>
2023-05-20 13:11:18 +0100

    plugin Provider package to app root

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+// import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:from_scratch/constants.dart';
+import 'package:from_scratch/models/auth.dart';
 import 'package:from_scratch/route/index.dart';
+import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
@@ -11,42 +17,50 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      theme: ThemeData(
-        fontFamily: 'Nunito',
-        // Elevated Button Theme
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            minimumSize: const Size.fromHeight(50),
-            backgroundColor: kGreen,
-            textStyle: const TextStyle(
-              fontFamily: 'Nunito',
-              fontSize: 16,
-              fontWeight: FontWeight.w700,
-            ),
+    return MultiProvider(
+        providers: [
+          ChangeNotifierProvider<AuthModel>(
+            create: (context) => AuthModel(),
           ),
-        ),
-        // Text Button Theme
-        textButtonTheme: TextButtonThemeData(
-          style: TextButton.styleFrom(
-            minimumSize: const Size.fromHeight(18),
-            foregroundColor: kGreen,
-            textStyle: const TextStyle(
-              fontSize: 16,
-              fontFamily: 'Nunito',
-              fontWeight: FontWeight.w700,
+        ],
+        child: MaterialApp.router(
+          theme: ThemeData(
+            fontFamily: 'Nunito',
+            // Elevated Button Theme
+            elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size.fromHeight(50),
+                backgroundColor: kGreen,
+                textStyle: const TextStyle(
+                  fontFamily: 'Nunito',
+                  fontSize: 16,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
             ),
+            // Text Button Theme
+            textButtonTheme: TextButtonThemeData(
+              style: TextButton.styleFrom(
+                minimumSize: const Size.fromHeight(18),
+                foregroundColor: kGreen,
+                textStyle: const TextStyle(
+                  fontSize: 16,
+                  fontFamily: 'Nunito',
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ),
+            // Text theme
+            textTheme: const TextTheme(
+                displayLarge: TextStyle(
+                    fontWeight: FontWeight.w700, fontSize: 24, color: kDark),
+                labelMedium:
+                    TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
+                bodySmall: TextStyle(fontSize: 14, fontWeight: FontWeight.w400),
+                bodyMedium:
+                    TextStyle(fontSize: 16, fontWeight: FontWeight.w400)),
           ),
-        ),
-        // Text theme
-        textTheme: const TextTheme(
-            displayLarge: TextStyle(
-                fontWeight: FontWeight.w700, fontSize: 24, color: kDark),
-            labelMedium: TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
-            bodySmall: TextStyle(fontSize: 14, fontWeight: FontWeight.w400),
-            bodyMedium: TextStyle(fontSize: 16, fontWeight: FontWeight.w400)),
-      ),
-      routerConfig: router,
-    );
+          routerConfig: router,
+        ));
   }
 }
