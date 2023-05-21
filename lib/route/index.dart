e9e5f6aa19885f8cfd93f8704a2933af066dd2dc
rockxzion <rock_zion@outlook.com>
2023-05-21 23:35:07 +0100

    add new route to home page

--- lib/route/index.dart
+++ lib/route/index.dart
@@ -1,8 +1,10 @@
+import 'package:from_scratch/screens/home_screen.dart';
 import 'package:from_scratch/screens/login_screen.dart';
 import 'package:from_scratch/screens/signup_screen.dart';
 import 'package:go_router/go_router.dart';
 
 GoRouter router = GoRouter(routes: [
+  GoRoute(path: "/", builder: (context, state) => const SignupScreen()),
   GoRoute(path: "/login", builder: (context, state) => const LoginScreen()),
-  GoRoute(path: "/", builder: (context, state) => const SignupScreen())
+  GoRoute(path: "/home", builder: (context, state) => const HomeScreen()),
 ]);
