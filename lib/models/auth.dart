670ae39eefeeadf024432474f29a7071293ef3d5
rockxzion <rock_zion@outlook.com>
2023-05-21 23:56:48 +0100

    Update auth.dart

--- lib/models/auth.dart
+++ lib/models/auth.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 
 class SignupModel extends ChangeNotifier {
-  late String _email;
-  late String _firstName;
-  late String _password;
+  late String _email = "";
+  late String _password = "";
 
   String get email => _email;
   set email(String email) {
@@ -13,11 +12,6 @@ class SignupModel extends ChangeNotifier {
   String get password => _password;
   set password(String password) {
     _password = password;
-  }
-
-  String get firstName => _firstName;
-  set firstName(String firstName) {
-    _firstName = firstName;
   }
 }
 
