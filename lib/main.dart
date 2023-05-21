b82c8109a9299fd7ffcc7aa00711c840f344199f
rockxzion <rock_zion@outlook.com>
2023-05-21 23:34:56 +0100

    instantiate firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 // import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:from_scratch/constants.dart';
 import 'package:from_scratch/models/auth.dart';
@@ -6,9 +7,8 @@ import 'package:from_scratch/route/index.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
-  // WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp();
-
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
