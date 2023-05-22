61b64f2c0dc6f1bd82ef74b63b8dcab394f26029
rockxzion <rock_zion@outlook.com>
2023-05-22 01:38:52 +0100

    incorporate new themes

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
 const kGrey = Color(0xff606060);
+const kStroke = Color(0xff363837);
 const kGreen = Color(0xff30BE76);
 const kDark = Color(0xff030F09);
+const kDivider = Color(0xffE6E6E6);
--- lib/main.dart
+++ lib/main.dart
@@ -28,41 +28,43 @@ class MyApp extends StatelessWidget {
         ],
         child: MaterialApp.router(
           theme: ThemeData(
-            fontFamily: 'Nunito',
-            // Elevated Button Theme
-            elevatedButtonTheme: ElevatedButtonThemeData(
-              style: ElevatedButton.styleFrom(
-                minimumSize: const Size.fromHeight(50),
-                backgroundColor: kGreen,
-                textStyle: const TextStyle(
-                  fontFamily: 'Nunito',
-                  fontSize: 16,
-                  fontWeight: FontWeight.w700,
+              fontFamily: 'Nunito',
+              // Elevated Button Theme
+              elevatedButtonTheme: ElevatedButtonThemeData(
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size.fromHeight(50),
+                  backgroundColor: kGreen,
+                  textStyle: const TextStyle(
+                    fontFamily: 'Nunito',
+                    fontSize: 16,
+                    fontWeight: FontWeight.w700,
+                  ),
                 ),
               ),
-            ),
-            // Text Button Theme
-            textButtonTheme: TextButtonThemeData(
-              style: TextButton.styleFrom(
-                minimumSize: const Size.fromHeight(18),
-                foregroundColor: kGreen,
-                textStyle: const TextStyle(
-                  fontSize: 16,
-                  fontFamily: 'Nunito',
-                  fontWeight: FontWeight.w700,
+              // Text Button Theme
+              textButtonTheme: TextButtonThemeData(
+                style: TextButton.styleFrom(
+                  minimumSize: const Size.fromHeight(18),
+                  foregroundColor: kGreen,
+                  textStyle: const TextStyle(
+                    fontSize: 16,
+                    fontFamily: 'Nunito',
+                    fontWeight: FontWeight.w700,
+                  ),
                 ),
               ),
-            ),
-            // Text theme
-            textTheme: const TextTheme(
+              // Text theme
+              textTheme: const TextTheme(
                 displayLarge: TextStyle(
                     fontWeight: FontWeight.w700, fontSize: 24, color: kDark),
                 labelMedium:
                     TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
                 bodySmall: TextStyle(fontSize: 14, fontWeight: FontWeight.w400),
                 bodyMedium:
-                    TextStyle(fontSize: 16, fontWeight: FontWeight.w400)),
-          ),
+                    TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
+              ),
+              dividerTheme:
+                  const DividerThemeData(color: kDivider, thickness: 1)),
           routerConfig: router,
         ));
   }
