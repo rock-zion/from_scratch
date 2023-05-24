adc7a418774cbb6424db103f4a14b6bc7953b113
rockxzion <rock_zion@outlook.com>
2023-05-24 13:17:06 +0100

    add profile page to route

--- lib/route/index.dart
+++ lib/route/index.dart
@@ -1,3 +1,4 @@
+import 'package:from_scratch/screens/edit_profile_screen.dart';
 import 'package:from_scratch/screens/home_screen.dart';
 import 'package:from_scratch/screens/login_screen.dart';
 import 'package:from_scratch/screens/signup_screen.dart';
@@ -7,4 +8,7 @@ GoRouter router = GoRouter(routes: [
   GoRoute(path: "/", builder: (context, state) => const SignupScreen()),
   GoRoute(path: "/login", builder: (context, state) => const LoginScreen()),
   GoRoute(path: "/home", builder: (context, state) => const HomeScreen()),
+  GoRoute(
+      path: "/editprofile",
+      builder: (context, state) => const EditProfileScreen()),
 ]);
--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -11,50 +11,105 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
+  bool _progressController = true;
+        _progressController = false;
+      });
+    } catch (e) {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
         child: Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 25),
-      child: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text("My Kitchen",
-                  style: Theme.of(context)
-                      .textTheme
-                      .displayLarge
-                      ?.copyWith(color: kDark)),
-              SizedBox(
-                width: 96,
-                // constraints: const BoxConstraints(minWidth: 77),
-                child: TextButton.icon(
-                  icon: SvgPicture.asset(
-                    "assets/images/icons/Settings.svg",
-                    width: 20,
-                    height: 20,
-                  ),
-                  onPressed: () {
-                    context.go("/settings");
-                  },
-                  label: Text(
-                    "Settings",
-                    style: Theme.of(context)
-                        .textTheme
-                        .bodySmall
-                        ?.copyWith(color: kDark),
-                  ),
+        child: _progressController
+            ? const Center(child: CircularProgressIndicator())
+            : Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("My Kitchen",
+                            style:
+                                textTheme.displayLarge?.copyWith(color: kDark)),
+                        SizedBox(
+                          width: 96,
+                          // constraints: const BoxConstraints(minWidth: 77),
+                          child: TextButton.icon(
+                            icon: SvgPicture.asset(
+                              "assets/images/icons/Settings.svg",
+                              width: 20,
+                              height: 20,
+                            ),
+                            onPressed: () {
+                              context.go("/settings");
+                            },
+                            label: Text(
+                              "Settings",
+                              style:
+                                  textTheme.bodySmall?.copyWith(color: kDark),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                    const SizedBox(height: 30),
+                    Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                CircleAvatar(
+                                  backgroundColor: kGreen,
+                                  radius: 41,
+                                  child: Text(
+                                    "${userData["fullName"]?.substring(0, 1)}",
+                                    style: textTheme.displayLarge
+                                        ?.copyWith(color: kWhite),
+                                  ),
+                                ),
+                                const SizedBox(width: 15),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    const SizedBox(height: 5),
+                                    Text("${userData["fullName"]}",
+                                        style: textTheme.bodyMedium?.copyWith(
+                                            fontWeight: FontWeight.w700)),
+                                    Text("Bio", style: textTheme.bodySmall),
+                                    const SizedBox(height: 11),
+                                    Row(
+                                      children: [
+                                        Text("584 followers",
+                                            style: textTheme.bodySmall),
+                                        const SizedBox(width: 10),
+                                        SvgPicture.asset(
+                                            "assets/images/icons/dot.svg"),
+                                        const SizedBox(width: 10),
+                                        Text("584 likes",
+                                            style: textTheme.bodySmall)
+                                      ],
+                                    )
+                                  ],
+                                ),
+                              ],
+                            ),
+                            IconButton(
+                                onPressed: () => context.go("/editprofile"),
+                                icon: SvgPicture.asset(
+                                    "assets/images/icons/Edit.svg"))
+                          ],
+                        ),
+                        const SizedBox(height: 26),
+                        const Divider()
+                      ],
+                    )
+                  ],
                 ),
               )
-            ],
-          ),
-          const SizedBox(height: 30),
-          Column(
-            children: const [Divider()],
-          )
-        ],
-      ),
-    ));
+              ));
   }
 }
