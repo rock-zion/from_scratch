aa71412f3f68a294c4daa65424bdca5f68f6dbab
rockxzion <rock_zion@outlook.com>
2023-05-24 20:50:28 +0100

    Create explore_screen.dart

---
+++ lib/screens/explore_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ExploreScreen extends StatefulWidget {
+  const ExploreScreen({Key? key}) : super(key: key);
+
+  @override
+  _ExploreScreenState createState() => _ExploreScreenState();
+}
+
+class _ExploreScreenState extends State<ExploreScreen>
+    with AutomaticKeepAliveClientMixin<ExploreScreen> {
+  @override
+  bool get wantKeepAlive => true;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
